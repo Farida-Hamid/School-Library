486c8deaa6fe33c2fefea9a94a5bf1b904b60b4a
Farida-Hamid <faridaaddala@gmail.com>
2022-11-15 17:57:41 +0300

    Create trimmer decorator

---
+++ trimmer_decorator.rb
@@ -0,0 +1,12 @@
+require './base_decorator'
+
+class TrimmerDecorator
+
+  def correct_name
+    if @nameable.correct_name.length <= 10
+      return @nameable.correct_name[0..10]
+    end
+
+    @nameable.correct_name
+  end
+end
