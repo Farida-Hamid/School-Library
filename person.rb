4fe892dd4b418c6eab055361b6a011471bc0981f
Farida-Hamid <faridaaddala@gmail.com>
2022-11-15 17:47:07 +0300

    Create base decorator

--- person.rb
+++ person.rb
@@ -5,6 +5,8 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
+
     @id = Random.rand(100)
     @age = age
     @name = name
