cb4bc09dfbb88b36ffc84be8e5178e1ee4326ca2
Farida-Hamid <faridaaddala@gmail.com>
2022-11-15 18:31:54 +0300

    Trim the eleventh letter

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,7 @@ require './base_decorator'
 
 class TrimmerDecorator < BaseDec
   def correct_name
-    return @nameable.correct_name[0..10] if @nameable.correct_name.length <= 10
+    return @nameable.correct_name[0..9] if @nameable.correct_name.length <= 10
 
     @nameable.correct_name
   end
