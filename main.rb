a9dcb2e6d5318a0099f75e775033764570a9309e
Farida-Hamid <faridaaddala@gmail.com>
2022-11-16 14:29:27 +0300

    Test the has-many side of Book and Rental

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require './person'
 require './student'
 require './classroom'
+require './book'
+require './rental'
 
 # Make sure that when adding a student to a classroom it also sets the classroom for the student.
 class_a = Classroom.new('A')
@@ -10,3 +12,11 @@ class_a.add_student(mark)
 print "\nMark's classroom is ", mark.classroom.label
 # Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
 print "\n", class_a.students, ' are enrolled in class ', class_A.label
+
+# Test books and rentals relationship
+book1 = Book.new('title1', 'author1')
+rental1 = Rental.new('now')
+puts rental1.date
+
+book1.add_rental(rental1)
+puts book1.rentals.length, rental1.book.title
