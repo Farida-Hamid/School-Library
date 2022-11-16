703f51d8b57c77166c393a90f2621bcdda15e3ab
Farida-Hamid <faridaaddala@gmail.com>
2022-11-16 15:32:09 +0300

    Define a setter method for classroom in class student

--- student.rb
+++ student.rb
@@ -1,15 +1,19 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age)
     super(age)
     @classroom = classroom
-    classroom.students << self
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self)
   end
 end
