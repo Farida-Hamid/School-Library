a595862b70ee82943d9d369bbd4c254339572774
Farida-Hamid <faridaaddala@gmail.com>
2022-11-18 15:59:05 +0300

    Create the app main structure

--- main.rb
+++ main.rb
@@ -3,21 +3,28 @@ require './student'
 require './classroom'
 require './book'
 require './rental'
+require './app'
 
-# Make sure that when adding a student to a classroom it also sets the classroom for the student.
-class_a = Classroom.new('A')
-mark = Student.new(class_a, 13, 'Mark')
-print "Mark's classroom is ", mark.name
-class_a.add_student(mark)
-print "\nMark's classroom is ", mark.classroom.label
-# Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
-print "\n", class_a.students, ' are enrolled in class ', class_a.label
+def main
+  app()
+end
 
-# Test books and rentals relationship
-book1 = Book.new('title1', 'author1')
-rental1 = Rental.new('now', book1, mark)
+main()
 
-puts book1.rentals.length, rental1.book.title
+# # Make sure that when adding a student to a classroom it also sets the classroom for the student.
+# class_a = Classroom.new('A')
+# mark = Student.new(class_a, 13, 'Mark')
+# print "Mark's classroom is ", mark.name
+# class_a.add_student(mark)
+# print "\nMark's classroom is ", mark.classroom.label
+# # Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
+# print "\n", class_a.students, ' are enrolled in class ', class_a.label
+
+# # Test books and rentals relationship
+# book1 = Book.new('title1', 'author1')
+# rental1 = Rental.new('now', book1, mark)
+
+# puts book1.rentals.length, rental1.book.title
 
 # # Test person and rentals relationship
 # person1 = Person.new(25, 'Person1')
