a7e641192fba6052c4bd4a08c7d7438ec784bd80
Farida-Hamid <faridaaddala@gmail.com>
2022-11-18 15:56:11 +0300

    Fix person bug

--- main.rb
+++ main.rb
@@ -6,8 +6,8 @@ require './rental'
 
 # Make sure that when adding a student to a classroom it also sets the classroom for the student.
 class_a = Classroom.new('A')
-mark = Student.new('s', 13)
-print "Mark's classroom is ", mark.classroom
+mark = Student.new(class_a, 13, 'Mark')
+print "Mark's classroom is ", mark.name
 class_a.add_student(mark)
 print "\nMark's classroom is ", mark.classroom.label
 # Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
@@ -15,16 +15,14 @@ print "\n", class_a.students, ' are enrolled in class ', class_a.label
 
 # Test books and rentals relationship
 book1 = Book.new('title1', 'author1')
-rental1 = Rental.new('now')
-puts rental1.date
+rental1 = Rental.new('now', book1, mark)
 
-book1.add_rental(rental1)
 puts book1.rentals.length, rental1.book.title
 
-# Test person and rentals relationship
-person1 = Person.new(25, name: 'Person1')
-rental2 = Rental.new('now2')
-puts rental2.date
+# # Test person and rentals relationship
+# person1 = Person.new(25, 'Person1')
+# rental2 = Rental.new('now2', book1, person1)
+# puts rental2.date
 
-person1.add_rental(rental2)
-puts person1.rentals.length, rental2.person.name
+# person1.add_rental(rental2)
+# puts person1.rentals.length, rental2.person.name
--- rental.rb
+++ rental.rb
@@ -1,8 +1,12 @@
 class Rental
-  attr_accessor :date, :book, :person
+  attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    @person = person
+
     book.rentals << self
     person.rentals << self
   end
